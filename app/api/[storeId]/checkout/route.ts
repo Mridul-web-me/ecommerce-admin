0939d4a82f6c9b81c52401a3acd85f4393a7644f
Mridul Islam <md.mridul.23@gmail.com>
2023-09-07 01:35:03 +0600

    payment gateway added, chart added, darm theme added

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -6,6 +6,7 @@ import prismadb from "@/lib/prismadb";
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": "true",
   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,6 +3,7 @@ import {MainNav} from "@/components/MainNav"
 import StoreSwitcher from "@/components/StoreSwitcher"
 import { redirect } from "next/navigation"
 import prismadb from "@/lib/prismadb"
+import { ThemeToggle } from "@/components/theme-toggle"
 
 const Navbar = async () => {
 
@@ -26,6 +27,7 @@ const Navbar = async () => {
             
             <MainNav className="mx-6"/>
             <div className="ml-auto flex items-center space-x-4">
+                <ThemeToggle/>
                 <UserButton afterSignOutUrl="" />
             </div> 
         </div>
