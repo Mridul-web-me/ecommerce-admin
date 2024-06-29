6298cac7706c7c9b6f051c86ae9a9fa19a2c403f
Mridul Islam <md.mridul.23@gmail.com>
2024-06-30 05:20:36 +0600

    node module updated

--- actions/getGaphRevenue.ts
+++ actions/getGaphRevenue.ts
@@ -1,25 +1,24 @@
-import prismadb from "@/lib/prismadb"
-
+import prismadb from '@/lib/prismadb';
 
 interface GraphData {
-    name: string;
-    total: number;
+  name: string;
+  total: number;
 }
 
 export const getGraphRevenue = async (storeId: string) => {
-    const paidOrders = await prismadb.order.findMany({
-        where: {
-            storeId,
-            isPaid: true,
-        },
+  const paidOrders = await prismadb.order.findMany({
+    where: {
+      storeId,
+      isPaid: true
+    },
+    include: {
+      orderItems: {
         include: {
-            orderItems: {
-                include: {
-                    product: true
-                }
-            }
+          product: true
         }
-    });
+      }
+    }
+  });
 
   const monthlyRevenue: { [key: number]: number } = {};
 
@@ -27,31 +26,31 @@ export const getGraphRevenue = async (storeId: string) => {
     const month = order.createdAt.getMonth();
     let revenueForOrder = 0;
 
-    for(const item of order.orderItems){
-        revenueForOrder += item.product.price.toNumber();
+    for (const item of order.orderItems) {
+      revenueForOrder += item.product.price || 0;
     }
 
-    monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder; 
-  };
+    monthlyRevenue[month] = (monthlyRevenue[month] || 0) + revenueForOrder;
+  }
 
   const graphData: GraphData[] = [
-    { name: "Jan", total: 0 },
-    { name: "Feb", total: 0 },
-    { name: "Mar", total: 0 },
-    { name: "Apr", total: 0 },
-    { name: "May", total: 0 },
-    { name: "Jun", total: 0 },
-    { name: "Jul", total: 0 },
-    { name: "Aug", total: 0 },
-    { name: "Sep", total: 0 },
-    { name: "Oct", total: 0 },
-    { name: "Nov", total: 0 },
-    { name: "Dec", total: 0 },
+    { name: 'Jan', total: 0 },
+    { name: 'Feb', total: 0 },
+    { name: 'Mar', total: 0 },
+    { name: 'Apr', total: 0 },
+    { name: 'May', total: 0 },
+    { name: 'Jun', total: 0 },
+    { name: 'Jul', total: 0 },
+    { name: 'Aug', total: 0 },
+    { name: 'Sep', total: 0 },
+    { name: 'Oct', total: 0 },
+    { name: 'Nov', total: 0 },
+    { name: 'Dec', total: 0 }
   ];
 
-  for(const month in monthlyRevenue){
+  for (const month in monthlyRevenue) {
     graphData[parseInt(month)].total = monthlyRevenue[parseInt(month)];
   }
 
-    return graphData;
+  return graphData;
 };
