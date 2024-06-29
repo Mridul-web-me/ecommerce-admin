9641799b33caf39342a11884b16d4aa786e670a8
Mridul Islam <md.mridul.23@gmail.com>
2024-06-30 02:40:58 +0600

    updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,36 @@
-import { UserButton, auth } from "@clerk/nextjs"
-import {MainNav} from "@/components/MainNav"
-import StoreSwitcher from "@/components/StoreSwitcher"
-import { redirect } from "next/navigation"
-import prismadb from "@/lib/prismadb"
-import { ThemeToggle } from "@/components/theme-toggle"
+import { UserButton, auth } from '@clerk/nextjs';
+import { MainNav } from '@/components/MainNav';
+import StoreSwitcher from '@/components/StoreSwitcher';
+import { redirect } from 'next/navigation';
+import prismadb from '@/lib/prismadb';
+import { ThemeToggle } from '@/components/theme-toggle';
 
 const Navbar = async () => {
+  const { userId } = auth();
 
-  const {userId} = auth()
-
-  if(!userId){
-    redirect("/sign-in");
+  if (!userId) {
+    redirect('/sign-in');
   }
 
   const stores = await prismadb.store.findMany({
     where: {
-      userId,
-
+      userId
     }
-  })
- 
+  });
+
   return (
     <div className="border-b">
-        <div className="flex h-16 items-center px-4">
-            <StoreSwitcher items={stores}/>
-            
-            <MainNav className="mx-6"/>
-            <div className="ml-auto flex items-center space-x-4">
-                <ThemeToggle/>
-                <UserButton afterSignOutUrl="" />
-            </div> 
-        </div>
-    </div>
-  )
-}
+      <div className="flex h-16 items-center px-4">
+        <StoreSwitcher items={stores} />
 
-export default Navbar
+        <MainNav className="mx-6" />
+        <div className="ml-auto flex items-center space-x-4">
+          <ThemeToggle />
+          <UserButton afterSignOutUrl="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
