c2255bcbb7c85b6743cffcded05b75aa36a90016
Mridul Islam <md.mridul.23@gmail.com>
2024-06-30 11:57:53 +0600

    update

--- app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/[orderId]/page.tsx
@@ -1,29 +1,24 @@
-import prismadb from "@/lib/prismadb"
-import OrderDelete from "./components/OrderDelete";
-import CellAction from "../components/cell-action";
+import prismadb from '@/lib/prismadb';
+import OrderDelete from './components/OrderDelete';
+import CellAction from '../components/cell-action';
 
-
-const OrderPage = async ({
-    params
-}: {
-    params: { orderId: string }
-}) => {
-    // fetch the data
-    const order = await prismadb.order.findUnique({
-        where: {
-            id: params.orderId
-        }
-    });
+const OrderPage = async ({ params }: { params: { orderId: string } }) => {
+  // fetch the data
+  const order = await prismadb.order.findUnique({
+    where: {
+      id: params.orderId
+    }
+  });
 
   return (
     <div className="flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">
+      {/* <div className="flex-1 space-y-4 p-8 pt-6">
             <OrderDelete
                 initialData={order}
             />
-        </div>
+        </div> */}
     </div>
-  )
-}
+  );
+};
 
-export default OrderPage
+export default OrderPage;
