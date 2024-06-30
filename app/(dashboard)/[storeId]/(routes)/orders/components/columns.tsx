968b27304fe6514bc1f72416775cbf9fecee868a
Mridul Islam <md.mridul.23@gmail.com>
2024-06-30 12:08:20 +0600

    order column update

--- app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
+++ app/(dashboard)/[storeId]/(routes)/orders/components/columns.tsx
@@ -1,47 +1,45 @@
-"use client"
+'use client';
 
-import { ColumnDef } from "@tanstack/react-table"
-import CellAction from "./cell-action";
+import { ColumnDef } from '@tanstack/react-table';
+import CellAction from './cell-action';
 
 export type OrderColumn = {
   id: string;
+  storeId: string;
+  isPaid: boolean;
   phone: string;
   address: string;
-  isPaid: boolean;
-  totalPrice: string;
-  products: string;
-  createdAt: string;
-}
+  createdAt: Date;
+  updatedAt: Date;
+};
 
 export const columns: ColumnDef<OrderColumn>[] = [
   {
-    accessorKey: "products",
-    header: "Products",
+    accessorKey: 'products',
+    header: 'Products'
   },
   {
-    accessorKey: "phone",
-    header: "Phone",
+    accessorKey: 'phone',
+    header: 'Phone'
   },
   {
-    accessorKey: "address",
-    header: "Address",
+    accessorKey: 'address',
+    header: 'Address'
   },
   {
-    accessorKey: "totalPrice",
-    header: "Total Price",
+    accessorKey: 'totalPrice',
+    header: 'Total Price'
   },
   {
-    accessorKey: "isPaid",
-    header: "Paid",
+    accessorKey: 'isPaid',
+    header: 'Paid'
   },
   {
-    accessorKey: "createdAt",
-    header: "Date",
+    accessorKey: 'createdAt',
+    header: 'Date'
   },
   {
-    accessorKey: "Actions",
-    cell: ({row}) => <CellAction data={row.original} />
+    accessorKey: 'Actions',
+    cell: ({ row }) => <CellAction data={row.original} />
   }
-]
-
-
+];
